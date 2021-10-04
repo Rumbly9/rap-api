eb0f44852deea96bcddc52a69e4b837eee017415
Aulia <aulachmadgmail.com>
2021-10-04 21:58:59 +0700

    fix PORT

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ app.get("/api/rappers/:rapperName", (request, response) => {
   }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`server running on port: ${PORT}`)
 })
